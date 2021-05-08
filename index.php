0c474b163091117387d79612b1b9f0cac7da3847
nicolasfueyo <nicolas93100.fueyo@gmail.com>
2021-05-08 19:54:33 +0200

    Mise en forme CSS des 2 pages avec Bootstrap.

--- index.php
+++ index.php
@@ -1,18 +1,30 @@
 <!doctype html>
 <html lang="en">
 <head>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8"
+            crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <link href="CSS/cvven-cli.css" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body>
-    <h1>Date de recherche : </h1>
+<body class="container">
+<h1 class="row justify-content-center titre">Recherche de disponiblité CVVEN :</h1>
+<div class="row">
     <form action="resultats.php" method="post">
-        <label>Date</label>
-        <input type="date" name="date">
-        <input type="submit">
+        <div class="mb-3">
+            <label for="date" class="form-label">Date</label>
+            <input type="date" class="form-control"  name="date">
+        </div>
+        <div class="mb-3">
+            <input type="submit" class="form-control" value="Envoyer">
+        </div>
     </form>
+</div>
 </body>
 </html>
--- resultats.php
+++ resultats.php
@@ -22,10 +22,16 @@ $dispos = $res->return;
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8"
+            crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <link href="CSS/cvven-cli.css" rel="stylesheet">
 </head>
-<body>
-    <h1>Disponibilités pour le <?php echo $date; ?></h1>
-    <table>
+<body class="container">
+    <h1 class="row titre justify-content-center">Disponibilités pour le <?php echo $date; ?></h1>
+    <table class="table table-striped">
         <thead>
             <th>Logement</th>
             <th>Disponible</th>
@@ -43,5 +49,6 @@ $dispos = $res->return;
         <?php endforeach; ?>
         </tbody>
     </table>
+    <a href="index.php" class="btn btn-primary">Retour</a>
 </body>
 </html>
